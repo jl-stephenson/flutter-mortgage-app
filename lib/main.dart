7be12d793cfde4fbcab9a5d587529568f4858bf4
John <johnlouisstephenson@gmail.com>
2024-09-23 16:26:14 +0100

    removed unnecessary library

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_mortgage_calc/services/calculation.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
-import 'dart:html';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
